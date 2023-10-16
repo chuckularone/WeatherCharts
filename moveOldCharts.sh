337be7cc02aedf749fe2bf99547d80db39214640
chuckularone <chuck@mckenna.pub>
2023-10-16 09:27:18 -0400

    fixed archiving of old charts

---
+++ moveOldCharts.sh
@@ -0,0 +1,4 @@
+/usr/bin/mv /var/www/html/image/humidity.png /var/www/html/image/archive/humidity_$(date +"%F").png
+/usr/bin/mv /var/www/html/image/pressure.png /var/www/html/image/archive/pressure_$(date +"%F").png
+/usr/bin/mv /var/www/html/image/temperature.png /var/www/html/image/archive/temperature_$(date +"%F").png
+
